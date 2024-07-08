8875256ba2da9db027c30d5bf578786a5ed6ac66
ko-ki2365 <161230299+ko-ki2365@users.noreply.github.com>
2024-07-08 23:03:25 +0900

    first commit

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+// 関数を定義
+const today = new Date();
+
+const year = today.getFullYear();
+const month = today.getMonth();
+const date = today.getDate();
+
+// 日付を出力
+console.log(year + "年" + month + "月" + date + "日");
